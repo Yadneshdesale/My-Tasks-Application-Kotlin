7b40f1876293065c06f87e31e35f423b46c25476
Yadneshdesale <yadneshdesale@gmail.com>
2022-07-27 16:35:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/splashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.todoapp
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(Runnable {
+            val i = Intent(this@splashActivity, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 2000)
+
+    }
+}
